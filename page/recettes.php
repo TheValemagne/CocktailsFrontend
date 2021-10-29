2777c6365f96ea8a8fd25e8e2c18e6bc4947f54c
Jeremy Houde <jeremy.houde@laposte.net>
2021-10-29 21:56:22 +0200

    Ajout d'une page recettes préférées

--- page/recettes.php
+++ page/recettes.php
@@ -1,7 +1,21 @@
-  <!-- Affichage des recettes préférées de l'utilisateur -->
     <main>
       <h1>Recettes préférées</h1>
 
       <ul>
+        <?php
+          if(isset($_SESSION["recettes"])){
+            $index = 0;
+
+            foreach ($_SESSION["recettes"] as $indice_recette) { // TODO: ajout de favorie en stockant l'indice de la recette
+              if($index > 0){ // indentation du code
+                echo "\t\t";
+              }
+
+              echo '<li><a href="index.php?page=recette&recette='.$indice_recette.'">'.$Recettes[$indice_recette]["titre"]."</a></li>\n";
+              $index++;
+            }
+          } else { ?><li>Auncune recette préférée pour l'instant</li>
+<?php } ?>
       </ul>
+
     </main>
